8a1ad11cab51245f118b2a8da8a4975d6adbac57
RafaelAguilar11 <rafaithaaguilar@gmail.com>
2026-03-08 00:37:06 -0600

    inicio de proyecto

--- 5ta compañia/Program.cs
+++ 5ta compañia/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿Console.WriteLine("hello world");
+Console.WriteLine("lista ");
+int[] nombres = new int[12];
